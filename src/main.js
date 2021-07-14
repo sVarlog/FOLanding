3bab048b3998976c7eadb7402e72c3df337bf86f
Mikhail <40181752+sVarlog@users.noreply.github.com>
2021-07-14 16:49:53 +0300

    Add main structure

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import App from './App.vue';
+import VueEllipseProgress from 'vue-ellipse-progress';
+
+Vue.use(VueEllipseProgress);
+
+
+Vue.config.productionTip = false
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
+
+window.Vue = Vue
